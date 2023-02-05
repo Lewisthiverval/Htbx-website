d669558cbe646fca45cde18fc54f9e0c562ff58b
Lewisthiverval <54368129+Lewisthiverval@users.noreply.github.com>
2023-02-05 01:55:45 +0000

    second-commit

---
+++ src/components/Promotix.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function PromoTix() {
+  return (
+    <div>
+      <iframe
+        title="link"
+        // style="width:100%"
+        height="500"
+        src="https://app.promotix.com/trade/select-tickets/HTBX---8-Years-tickets?embedded_to_iframe=true"
+        frameBorder="0"
+      ></iframe>
+    </div>
+  );
+}
